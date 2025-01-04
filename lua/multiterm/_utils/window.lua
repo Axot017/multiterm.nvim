59f27db5988630321545fbbcf9cd895beb3b2525
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-01-04 13:20:58 +0100

    feat: Allow to modify window opts

--- lua/multiterm/_utils/window.lua
+++ lua/multiterm/_utils/window.lua
@@ -3,22 +3,9 @@ local logger = require("multiterm._utils.logger")
 local M = {}
 
 M.create = function(buffer, opts)
-  local width = opts.width or math.floor(vim.o.columns * 0.8)
-  local height = opts.height or math.floor(vim.o.lines * 0.8)
+  logger.log(logger.levels.DEBUG, "Creating window with config: " .. vim.inspect(opts))
 
-  local window_config = {
-    relative = "editor",
-    width = width,
-    height = height,
-    style = "minimal",
-    border = "rounded",
-    row = math.floor((vim.o.lines - height) / 2),
-    col = math.floor((vim.o.columns - width) / 2),
-  }
-
-  logger.log(logger.levels.DEBUG, "Creating window with config: " .. vim.inspect(window_config))
-
-  local window = vim.api.nvim_open_win(buffer, true, window_config)
+  local window = vim.api.nvim_open_win(buffer, true, opts)
 
   return window
 end
--- lua/multiterm/config.lua
+++ lua/multiterm/config.lua
@@ -1,8 +1,19 @@
 local M = {}
 
+local default_width = math.floor(vim.o.columns * 0.8)
+local default_height = math.floor(vim.o.lines * 0.8)
+
 local _default_config = {
   log_level = 4,
-  window = {}
+  window = {
+    relative = "editor",
+    width = default_width,
+    height = default_height,
+    style = "minimal",
+    border = "rounded",
+    row = math.floor((vim.o.lines - default_height) / 2),
+    col = math.floor((vim.o.columns - default_width) / 2),
+  }
 }
 
 local _config = _default_config
