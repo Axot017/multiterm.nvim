8ebb2c7a5a42a943598b3509d832c844c1d462be
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-09 23:04:27 +0100

    feat: Add missing commands

--- plugin/multiterm.lua
+++ plugin/multiterm.lua
@@ -7,11 +7,25 @@ vim.api.nvim_create_user_command("MultitermToggle",
     nargs = 1,
   })
 
+vim.api.nvim_create_user_command("MultitermBindToggle",
+  function()
+    multiterm.bind_toggle()
+  end, {
+    nargs = 0,
+  })
+
 vim.api.nvim_create_user_command("MultitermRemove",
   function(opts)
     multiterm.remove(opts.fargs[1])
   end, {
     nargs = 1,
+  })
+
+vim.api.nvim_create_user_command("MultitermBindRemove",
+  function()
+    multiterm.bind_remove()
+  end, {
+    nargs = 0,
   })
 
 vim.api.nvim_create_user_command("MultitermRemoveAll",
@@ -26,4 +40,20 @@ vim.api.nvim_create_user_command("MultitermCloseActive",
     multiterm.close_active()
   end, {
     nargs = 0,
+  })
+
+vim.api.nvim_create_user_command("MultitermSendSelection",
+  function(opts)
+    multiterm.send_selection(opts.fargs[1])
+  end, {
+    nargs = 1,
+    range = true,
+  })
+
+vim.api.nvim_create_user_command("MultitermBindSendSelection",
+  function()
+    multiterm.bind_send_selection()
+  end, {
+    nargs = 0,
+    range = true,
   })
