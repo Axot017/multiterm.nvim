2d3a4eae14c69a6e14993d9855c96dfe376501e4
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-03-11 07:28:51 +0100

    feat: add user feedback for key binding operations (#6)

--- lua/multiterm/_utils/binder.lua
+++ lua/multiterm/_utils/binder.lua
@@ -2,7 +2,9 @@ local M = {}
 
 M.bind = function(fn)
   vim.schedule(function()
+    vim.api.nvim_echo({ { "Waiting for key input...", "WarningMsg" } }, false, {})
     local key = vim.fn.getchar()
+    vim.api.nvim_echo({ { '', '' } }, false, {})
     local binding = vim.fn.nr2char(key)
     fn(binding)
   end)
